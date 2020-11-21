a846b2f043bb459547d670872f64eebd82f248f9
Mikhail <16112000m@gmail.com>
2020-11-21 23:53:42 +0300

    - big adjustment to algorithm

---
+++ additionalFunction.py
@@ -0,0 +1,164 @@
+import math
+
+import cv2
+import numpy as np
+
+TEMP = "tmp/"
+
+
+def saveFile(file, fileName):
+    cv2.imwrite(TEMP + fileName, file)
+
+
+def saveFileFinal(file, fileName):
+    cv2.imwrite(fileName, file)
+
+
+def canny(img, low_threshold, high_threshold):
+    return cv2.Canny(img, low_threshold, high_threshold)
+
+
+def get_hough_lines(img, rho=1, theta=np.pi / 180, threshold=90, min_line_len=130, max_line_gap=8):
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]),
+                            minLineLength=min_line_len, maxLineGap=max_line_gap)
+    return lines
+
+
+def draw_lines(img, lines, height, color=None, thickness=2):
+    # print(height)
+    if color is None:
+        color = [0, 0, 255]
+        color2 = [0, 255, 0]
+    vertical_lines = []
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            vertical = (math.sqrt(abs(y2 ** 2 - y1 ** 2)) > math.sqrt(abs(x2 ** 2 - x1 ** 2))) > 0
+            # print(vertical, x1, y1, x2, y2)
+            if vertical:
+                vertical_lines.append(line)
+        # cv2.line(img, (x1, y1), (x2, y2), color2, thickness)
+    min_y = height
+
+    # for i in range(len(vertical_lines)):
+    #    if vertical_lines[i][0][3] < vertical_lines[i][0][1]:
+    #        tmp_y = vertical_lines[i][0][3]
+    #        tmp_x = vertical_lines[i][0][2]
+    #        vertical_lines[i][0][3] = vertical_lines[i][0][1]
+    #        vertical_lines[i][0][2] = vertical_lines[i][0][0]
+    #        vertical_lines[i][0][1] = tmp_y
+    #        vertical_lines[i][0][0] = tmp_x
+
+    xCoordGroup = []
+    delList = []
+    for i in range(len(vertical_lines)):
+        # print(vertical_lines[i][0])
+        if not xCoordGroup:
+            # print("Group", len(xCoordGroup))
+            xCoordGroup.append(i)
+        else:
+            changed = False
+            for a in range(len(xCoordGroup)):
+                group = xCoordGroup[a]
+                midC = (vertical_lines[i][0][2] + vertical_lines[i][0][0]) / 2
+                midG = (vertical_lines[group][0][2] + vertical_lines[group][0][0]) / 2
+                if (abs(midG - midC) < 90) and \
+                        ((vertical_lines[i][0][1] - vertical_lines[i][0][3])
+                         * (vertical_lines[group][0][1] - vertical_lines[group][0][3]) > 0):
+                    # print("Group", group)
+                    minY2 = min(vertical_lines[i][0][3], vertical_lines[group][0][3])
+                    minY1 = min(vertical_lines[i][0][1], vertical_lines[group][0][1])
+                    vertical_lines[group][0][0] = x1 = (vertical_lines[i][0][0] + vertical_lines[group][0][0]) / 2
+                    vertical_lines[group][0][1] = y1 = (vertical_lines[i][0][1] + vertical_lines[group][0][1]) / 2
+                    vertical_lines[group][0][2] = x2 = (vertical_lines[i][0][2] + vertical_lines[group][0][2]) / 2
+                    vertical_lines[group][0][3] = y2 = (vertical_lines[i][0][3] + vertical_lines[group][0][3]) / 2
+                    # print(x1, y1, x2, y2)
+                    if x2 != x1:
+                        if y1 < y2:
+                            k = (y2 - y1) / (x2 - x1)
+                            c = y1 - k * x1
+                            vertical_lines[group][0][1] = minY1
+                            vertical_lines[group][0][0] = (minY1 - c) / k
+                            delList.append(i)
+                            changed = True
+                            break
+                        else:
+                            k = (y2 - y1) / (x2 - x1)
+                            c = y1 - k * x1
+                            vertical_lines[group][0][3] = minY2
+                            vertical_lines[group][0][2] = (minY2 - c) / k
+                            delList.append(i)
+                            changed = True
+                            break
+                    else:
+                        if y1 < y2:
+                            vertical_lines[group][0][3] = minY1
+                            delList.append(i)
+                            changed = True
+                            break
+                        else:
+                            vertical_lines[group][0][1] = minY2
+                            delList.append(i)
+                            changed = True
+                            break
+            if not changed:
+                # print("Group", len(xCoordGroup))
+                xCoordGroup.append(i)
+
+    i = 0
+    for toDel in delList:
+        vertical_lines.pop(toDel - i)
+        i += 1
+
+    for line in vertical_lines:
+        for x1, y1, x2, y2 in line:
+            if y2 < min_y:
+                min_y = y2
+            if y1 < min_y:
+                min_y = y1
+
+    # min_y = min_y - (height - min_y) * 0.1
+
+    for line in vertical_lines:
+        for x1, y1, x2, y2 in line:
+            if x2 != x1:
+                k = (y2 - y1) / (x2 - x1)
+                c = y1 - k * x1
+                if y2 > y1:
+                    y2 = int(height)
+                    x2 = int((y2 - c) / k)
+                    y1 = int(min_y)
+                    x1 = int((y1 - c) / k)
+                else:
+                    y1 = int(height)
+                    x1 = int((y1 - c) / k)
+                    y2 = int(min_y)
+                    x2 = int((y2 - c) / k)
+            else:
+                if y2 > y1:
+                    y2 = int(height)
+                    y1 = int(min_y)
+                else:
+                    y1 = int(height)
+                    y2 = int(min_y)
+
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+
+    return img
+
+
+def get_aoi(img):
+    rows, cols = img.shape[:2]
+    mask = np.zeros_like(img)
+
+    left_bottom = [cols * 0.1, rows]
+    right_bottom = [cols * 0.95, rows]
+    left_top = [cols * 0.45, rows * 0.4]
+    right_top = [cols * 0.55, rows * 0.4]
+
+    vertices = np.array([[left_bottom, left_top, right_top, right_bottom]], dtype=np.int32)
+
+    if len(mask.shape) == 2:
+        cv2.fillPoly(mask, vertices, 255)
+    else:
+        cv2.fillPoly(mask, vertices, (255,) * mask.shape[2])
+    return cv2.bitwise_and(img, mask)
---
+++ design.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_MainWindow(object):
+
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("RailWay finder")
+        MainWindow.resize(683, 686)
+
+        self.mw = MainWindow
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.label.setScaledContents(True)
+        self.verticalLayout.addWidget(self.label)
+
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setObjectName("listWidget")
+        self.verticalLayout.addWidget(self.listWidget)
+
+        self.fileButton = QtWidgets.QPushButton(self.centralwidget)
+        self.fileButton.setObjectName("fileButton")
+        self.verticalLayout.addWidget(self.fileButton)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("RailWay finder", "RailWay finder"))
+        self.label.setText(_translate("RailWay finder", "Здесь будет ваше изображение"))
+        self.fileButton.setText(_translate("RailWay finder", "Выберите файл"))
